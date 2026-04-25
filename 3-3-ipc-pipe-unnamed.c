8c6dbd37e60d41fb2bc36c50df291734b499cd65
lotarkiss <144259980+lotarkiss@users.noreply.github.com>
2026-04-25 21:47:59 +0200

    cleam

--- 3-3-ipc-pipe-unnamed.c
+++ 3-3-ipc-pipe-unnamed.c
@@ -35,7 +35,7 @@ int main()
                 printf("%d: Parent process\n", getpid());
                 close(pipefd[0]); // Parent only writes
 
-                printf("%d: Writing to the pipe\n: ", getpid(), getpid());
+                printf("%d: Writing to the pipe\n: ", getpid());
 
                 // File handling like a pipe
                 char buffer[100];
